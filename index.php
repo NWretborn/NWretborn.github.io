baa8c3aac4c0a7e9f3800945c17a6ac241c6c9b0
Jakob Norell <jakobnorell@gmail.com>
2016-11-16 18:30:39 +0100

    cachedcsslogo

--- index.php
+++ index.php
@@ -1,103 +1,16 @@
 <!DOCTYPE html>
 <html>
-<style>
-	body{
-		background-color: black;
-		/* background-image: url("index_background.png"); */
-		background-size:cover;
-	}
-</style>
+
 <head>
-<meta http-equiv="Content-Language" content="sv">
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-<title>WiFinder</title>
-<style type="text/css">
-	#c{	text-align: center;	}
-	#l{	text-align: left;	}
-	#r{	text-align: right;	}
-	
-	/* Rounded Corner */
-	.tb5 {
-		background-color: #10454e;
-		border:1px solid #10454e;
-		border-radius:20px;
-		font-size:1.5vw;
-		font-family: monospace;
-		width: 143%;
-	}
-	.tb4 {
-		
-		background-color: #10454e;
-		border:0px solid #29afc4;
-		border-radius:20px;
-		font-size: 1.5vw;
-	}
-	.center{
-		
-		left: 50%;
-		top: 50%;
-		transform: translate(-50%, -50%);
-	}
-	.hCenter{
-		left: 58.5%;
-		transform: translate(-50%, 0);
-	}
-	div{
-		font-size:1.5vw;
-		position: fixed;
-	}
-	.box{
-		height: 20vh;
-		color: #79969a;
-		font-family: monospace;
-		background-color: black;
-		width: 30vw;
-		border: 2px solid #10454e;
-		border-radius:10px;
-		padding: 20px;
-	}
-	.tb4:hover{
-		border:2px solid #29afc4;
-		border-radius: 20px;
-	}
-	input:focus {
-		border: 2px solid #29afc4;
-		border-radius: 20px;
-		outline: none;
-	}
-	.logo{
-		position: fixed;
-		height:15vh;
-		left:8%;
-		bottom:8vw;
-		transform: translate(-50%, 50%);
-	}
-</style>
+	<meta http-equiv="Content-Language" content="sv">
+	<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+	<title>WiFinder</title>
+	<link rel="stylesheet" type="text/css" href="libs/wifinder.css">
 
 </head>
-<script type="text/javascript">
-	function changePic(element, imageName) {
-    	element.setAttribute('src', imageName);
-	}
-	
-	function larger(element){
-		var style = window.getComputedStyle(element),
-		h = parseFloat(style.height),
-		vH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0); // MOBILE SCALING!!!
-		h = (h*1.2)/vH*100;
-		element.style.height = h + 'vh';
-	}
-	
-	
-	function smaller(element){
-		var style = window.getComputedStyle(element),
-		h = parseFloat(style.height),
-		vH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0); // MOBILE SCALING!!!
-		h = (h/1.2)/vH*100;
-		element.style.height = h + 'vh';
-	}
-	
-</script>
+
+<script src="libs/std.js"></script>
+
 <body>
 
 	<div class="center box">
@@ -133,11 +46,10 @@
 			
 	</div>
 	
-	<img src="logo_blue.png" class="logo" 
-		onmouseover="changePic(this, 'logo_about.png'); larger(this);" 
-		onmouseout="changePic(this, 'logo_blue.png'); smaller(this);"
-		onclick="location.href='about.php';"
-	>
+
+
+	<span class="logo"> </span>
+	<span class="cache" style="background: url(img/logo_about_gray.png)"></span>
 	
 </body>
 </html>
