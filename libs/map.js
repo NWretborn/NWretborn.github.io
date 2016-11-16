2e18379a6f41292a87cde9be1a23d14257b0ff3b
Jakob Norell <jakobnorell@gmail.com>
2016-11-16 19:07:26 +0100

    libs/map.php

--- libs/map.js
+++ libs/map.js
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-  <title>WiFinder</title>
-  <link rel="stylesheet" type="text/css" href="libs/wifinder.css">
-</head>
-
-<script src="libs/std.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJR14TQYMP-yBtsSpULmOe0hM7bHPWasQ"
-        type="text/javascript"></script>
-<script src="libs/map.js"></script>
-<!--
-<script type="text/javascript">
-  //<![CDATA[
-
-  var customIcons = {
+var customIcons = {
     restaurant: {
       icon: 'http://labs.google.com/ridefinder/images/mm_20_blue.png'
     },
@@ -78,21 +62,3 @@
   }
 
   function doNothing() {}
-
-  //]]>
-</script>
--->
-
-
-
-<body onload="load()">
-  <div id="map" style="width: 300px; height: 500px;"></div>
-  
-<img src="logo_gray.png" class="logo" 
-	onmouseover="changePic(this, 'logo_blue.png'); larger(this);" 
-	onmouseout="changePic(this, 'logo_gray.png'); smaller(this);"
-	onclick="location.href='index.php';"
->
-</body>
-
-</html>
