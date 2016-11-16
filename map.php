968934807ca4569f00fa9d0c299e92e6501b1d77
NWretborn <niclas.vretborn@gmail.com>
2016-11-16 16:50:54 +0100

    Update map.php

--- map.php
+++ map.php
@@ -37,6 +37,22 @@
 		element.style.height = h + 'vh';
 	}
 	
+	function downloadUrl(url,callback) {
+ 		var request = window.ActiveXObject ?
+     		new ActiveXObject('Microsoft.XMLHTTP') :
+     		new XMLHttpRequest;
+
+ 		request.onreadystatechange = function() {
+   			if (request.readyState == 4) {
+     				request.onreadystatechange = doNothing;
+     				callback(request, request.status);
+   				}
+ 	};
+
+	request.open('GET', url, true);
+ 	request.send(null);
+	}
+	
 </script>
 <head>
 	<meta http-equiv="Content-Language" content="sv">
