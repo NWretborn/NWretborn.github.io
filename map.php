0a359b23d8a6f52f883b34de7ce3c2373290d846
NWretborn <niclas.vretborn@gmail.com>
2016-11-17 17:44:54 +0100

    Update map.php

--- map.php
+++ map.php
@@ -13,7 +13,7 @@
 
 
 <body onload="load()">
-  <div id="map" style="width: 300px; height: 500px;"></div>
+  <div id="map" style="width: 60%; height: 100%;"></div>
   
 <img src="logo_gray.png" class="logo" 
 	onmouseover="changePic(this, 'logo_blue.png'); larger(this);" 
