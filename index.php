d09565ec3c6981882302d60d84ef3bcf691cd87c
Jakob Norell <jakobnorell@gmail.com>
2016-11-17 21:51:57 +0100

    indexandCSS

--- index.php
+++ index.php
@@ -6,52 +6,54 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
 	<title>WiFinder</title>
 	<link rel="stylesheet" type="text/css" href="libs/wifinder.css">
-
+	
 </head>
+
+
 
 <script src="libs/std.js"></script>
 
 <body>
 
-	<div class="center box">
-			<div style="top: 12%; left: 8%">
-				Username
-			</div>
-			<div class="hCenter" style="top: 12%">
-				<input id="c" type="text" name="Fname" class="tb5">
-			</div>
-			<br/>
-			<div style="top: 32%; left: 8%;">
-				Password
-			</div>
-			<div class="hCenter" style="top: 32%">
-				<input id="c" type="password" name="Fname" class="tb5">
-			</div>
-			<br/>
-			<div class="center" style="top:65%; left: 89%;">
-			<form action="map.php" method="get">
-				<input type="submit" value="Log in" name="register" class="tb4">
-				</form>
-			</div>
-			<div class="center" style="top:65%; left: 58%;">
-			<form action="map.php" method="get">
-				<input type="submit" value="Continue as guest" name="register" class="tb4">
-				</form>
-			</div>
-			<div class="center" style="top:101%;">
-			<form action="signup.php" method="get">
-				<input type="submit" value="Sign up" name="register" class="tb4">
-				</form>
-			</div>
-			
-	</div>
-	
 
+	<div class="box center">
+		<span class="text_box">
+
+			<span class="left text" style="top:calc(var(--FONT_HEIGHT)*1);">
+				Username
+			</span>
+			<input type="text" name="username" class="right" style="top:calc(var(--FONT_HEIGHT)*1); width:50%;">
+
+			<span class="left text" style="top:calc(var(--FONT_HEIGHT)*3);">
+				Password
+			</span>
+			<input type="password" name="password" class="right" style="top:calc(var(--FONT_HEIGHT)*3); width:50%;">
+			
+			<form action="map.php" method="get">
+				<input type="submit" value="Log in" name="register" class="right" style="top:calc(var(--FONT_HEIGHT)*6);">
+			</form>
+
+			<form action="map.php" method="get">
+				<input type="submit" value="Continue as guest" name="register" style="top:calc(var(--FONT_HEIGHT)*6); left:48%;">
+			</form>
+		</span>
+
+		<form action="signup.php" method="get">
+			<input type="submit" value="Sign up" name="register" class="hCenter centerpoint" style="position:absolute; top:100%;" >
+		</form>
+	</div>	
+	
 
 	<span class="logo" onclick="location.href='about.php'"> </span>
 	<span class="cache" style="background: url(img/logo_about_gray.png)"></span>
+
 	
 </body>
 </html>
 
+<!--
 
+	style="top: 12%; left: 8%"			style="top: 12%"
+	style="top: 32%; left: 8%;"			style="top: 32%"
+	style="top:65%; left: 89%;"			style="top:65%; left: 58%;"
+-->
