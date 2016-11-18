c74d6eb2ddc3e495c84dd2571d0f15358ebbeb59
NWretborn <niclas.vretborn@gmail.com>
2016-11-18 15:56:41 +0100

    Update map.js

--- libs/map.js
+++ libs/map.js
@@ -11,7 +11,7 @@ var customIcons = {
     var map = new google.maps.Map(document.getElementById("map"), {
       center: new google.maps.LatLng(65.618080, 22.140047),
       zoom: 10,
-      mapTypeId: 'roadmap'
+      mapTypeId: 'hybrid'
     });
     var infoWindow = new google.maps.InfoWindow;
 
