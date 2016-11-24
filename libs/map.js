4ce26667db1d1496b5194409b1154626d95d599b
NWretborn <niclas.vretborn@gmail.com>
2016-11-24 19:11:13 +0100

    Update map.js

--- libs/map.js
+++ libs/map.js
@@ -1,19 +1,20 @@
-function writeAddressName(latLng) {
-                var geocoder = new google.maps.Geocoder();
-                geocoder.geocode({
-                "location": latLng
-                },
-function geolocationSuccess(position) {
-                var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                // Write the formatted address
-                writeAddressName(userLatLng);
-function(results, status) {
-                if (status == google.maps.GeocoderStatus.OK)
-                document.getElementById("address").innerHTML = results[0].formatted_address;
+ function writeAddressName(latLng) {
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({
+          "location": latLng
+        },
+        function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK)
+            document.getElementById("address").innerHTML = results[0].formatted_address;
           else
-                document.getElementById("error").innerHTML += "Unable to retrieve your address" + "<br />";
-                });
-        }
+            document.getElementById("error").innerHTML += "Unable to retrieve your address" + "<br />";
+        });
+      }
+
+      function geolocationSuccess(position) {
+        var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        // Write the formatted address
+        writeAddressName(userLatLng);
 
 function load() {
     var map = new google.maps.Map(document.getElementById("map"), {
