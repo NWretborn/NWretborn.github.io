082b15bad254702f24503aab8339fef0f2c827fa
NWretborn <niclas.vretborn@gmail.com>
2016-11-28 00:26:44 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -105,7 +105,7 @@
 
 
 	<div class="center box">
-		<form action="http://213.113.7.224/confirmed.php" method="http://213.113.7.224/api.php/usr?method=POST">
+		<form action="http://213.113.7.224/confirmed.php" method="http://213.113.7.224/api.php?method=POST/usr">
 			<div style="top: 12%; left: 8%">
 				Username
 			</div>
