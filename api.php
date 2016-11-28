92b9e84bc79eb63f2882f58f2238660d2b1c0826
NWretborn <niclas.vretborn@gmail.com>
2016-11-28 15:07:23 +0100

    Update api.php

--- api.php
+++ api.php
@@ -6,7 +6,7 @@ session_start();
 
 // get the HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
-$request = explode('/', trim($_SERVER['213.113.7.224'],'/'));
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
  
 // connect to the mysql database
