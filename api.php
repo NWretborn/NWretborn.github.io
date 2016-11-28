8366c62a3467ebf010b429948533401fc381a096
NWretborn <niclas.vretborn@gmail.com>
2016-11-28 15:21:41 +0100

    Update api.php

--- api.php
+++ api.php
@@ -2,7 +2,7 @@
  
 require("phpsqlajax_dbinfo.php");
 
-
+echo $localhost
 // get the HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
