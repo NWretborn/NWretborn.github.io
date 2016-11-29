181760c77af8e6e4362b02d9168424482c2aab97
NWretborn <niclas.vretborn@gmail.com>
2016-11-29 16:51:34 +0100

    Update api2.php

--- api2.php
+++ api2.php
@@ -4,7 +4,7 @@ require("phpsqlajax_dbinfo.php");
 
 // get the HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
-$request = explode(trim('/',$_SERVER['PATH_INFO'],'/'));
+$request = explode(trim('/',$_SERVER['PATH_INFO']));
 $input = json_decode(file_get_contents('php://input'),true);
  
 // connect to the mysql database
