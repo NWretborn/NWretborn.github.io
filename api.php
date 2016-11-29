27d61489e655dca951862512d695f4ff17d55460
NWretborn <niclas.vretborn@gmail.com>
2016-11-29 16:28:31 +0100

    Update api.php

--- api.php
+++ api.php
@@ -7,4 +7,8 @@ $result = $mysqli->query("SELECT 'Hello, dear MySQL user!' AS _message FROM DUAL
 $row = $result->fetch_assoc();
 echo htmlentities($row['_message']);
 
+$method = $_SERVER['REQUEST_METHOD'];
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+$input = json_decode(file_get_contents('php://input'),true);
+
 ?>
