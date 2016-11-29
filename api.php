667ce05f66d73218a51760184b033224aacbae7e
NWretborn <niclas.vretborn@gmail.com>
2016-11-29 16:25:55 +0100

    Update api.php

--- api.php
+++ api.php
@@ -2,7 +2,7 @@
 
 require("phpsqlajax_dbinfo.php");
 
-$mysqli = new mysqli($localhost, $username, $password, $database);
+$mysqli = new mysqli('localhost', $username, $password, $database);
 $result = $mysqli->query("SELECT 'Hello, dear MySQL user!' AS _message FROM DUAL");
 $row = $result->fetch_assoc();
 echo htmlentities($row['_message']);
