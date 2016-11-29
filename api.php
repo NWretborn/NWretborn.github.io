28ecb955d20079bb00baf70caa886c02b5f00f2f
NWretborn <niclas.vretborn@gmail.com>
2016-11-29 16:22:01 +0100

    Create api.php

---
+++ api.php
@@ -0,0 +1,10 @@
+<?php
+
+require phpsqlajax_dbinfo.php
+
+$mysqli = new mysqli(localhost, user, password, database);
+$result = $mysqli->query("SELECT 'Hello, dear MySQL user!' AS _message FROM DUAL");
+$row = $result->fetch_assoc();
+echo htmlentities($row['_message']);
+
+?>
