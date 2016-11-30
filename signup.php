e0c4c0a4a05d52135fb871e3083de8481ea1d8b2
NWretborn <niclas.vretborn@gmail.com>
2016-11-30 16:40:24 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -102,7 +102,7 @@
 
 
 	<div class="center box">
-		<form action="confirmed.php" method="post user">
+		<form action="confirmed.php" method="post">
 			<div style="top: 12%; left: 8%">
 				Username
 			</div>
