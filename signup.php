b258cad2243e181f477d9bab083efa202991c44e
NWretborn <niclas.vretborn@gmail.com>
2016-11-30 16:15:30 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -102,7 +102,7 @@
 
 
 	<div class="center box">
-		<form action="http://213.113.7.224/confirmed.php" method="post api.php/user">
+		<form action="" method="post api.php/user">
 			<div style="top: 12%; left: 8%">
 				Username
 			</div>
