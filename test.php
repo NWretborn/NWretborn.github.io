65096c639ff27ca7d8a0b08134ef0b4755ce2016
NWretborn <niclas.vretborn@gmail.com>
2016-12-01 17:58:29 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-echo $_SERVER['PATH_INFO']
-echo explode('/', trim($_SERVER['PHP_USER'],'/'));
+echo ($_SERVER['PATH_INFO']);
+echo (explode('/', trim($_SERVER['PHP_USER'],'/')));
 #echo preg_replace('/[^a-z0-9_]+/i','',array_shift(explode('/', trim($_SERVER['PATH_INFO'],'/'));));
 ?>
