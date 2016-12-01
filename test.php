fae5b59a0f224994301892dd36971259c750bc32
NWretborn <niclas.vretborn@gmail.com>
2016-12-01 17:56:42 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,5 @@
 <?php
-
-echo $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-
-echo $request = explode('/', trim($_SERVER['PHP_USER'],'/'));
-echo $table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
-
-
+echo explode('/', trim($_SERVER['PATH_INFO'],'/'));
+echo explode('/', trim($_SERVER['PHP_USER'],'/'));
+echo preg_replace('/[^a-z0-9_]+/i','',array_shift(explode('/', trim($_SERVER['PATH_INFO'],'/'));));
 ?>
