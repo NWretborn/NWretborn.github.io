90b6599cd19688588be5ce16b71ab4310607161f
NWretborn <niclas.vretborn@gmail.com>
2016-12-01 17:11:49 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -102,7 +102,7 @@
 
 
 	<div class="center box">
-		<form action="api.php/user" method="post api.php/user">
+		<form action="<?php echo $request = explode('/', trim($_SERVER['PATH_INFO'],'/')); ?>" method="post api.php/user">
 			<div style="top: 12%; left: 8%">
 				Username
 			</div>
