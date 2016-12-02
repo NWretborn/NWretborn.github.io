c69d3d49bc7bc66c7d122b9e8540e57d220482d5
NWretborn <niclas.vretborn@gmail.com>
2016-12-02 16:13:59 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -103,7 +103,7 @@
 
 
 	<div class="center box">
-		<form action="<?php echo json_decode(file_get_contents('php://input'),true);?>" method="post">
+		<form action="<?php echo $_SERVER['PATH_INFO'];?>" method="post">
 		
 			<div style="top: 12%; left: 8%">
 				Username
