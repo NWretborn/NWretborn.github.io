e97e29d9ba51d8dc8af99a4e1b90773afbe2ab23
jakkarn <jakobnorell@gmail.com>
2016-12-02 15:36:27 +0100

    Update api.php

--- api.php
+++ api.php
@@ -59,6 +59,7 @@ if (!$result) {
 
 // print results, insert id or affected row count
 if ($method == 'GET') {
+  echo "GET!!!!!!!!!";
   if (!$key) echo '[';
   for ($i=0;$i<mysqli_num_rows($result);$i++) {
     echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
