4a4c96a675ddf464ac2da4a3e53b5931764e0d6d
jakkarn <jakobnorell@gmail.com>
2016-12-05 16:02:58 +0100

    Update api.php

--- api.php
+++ api.php
@@ -8,6 +8,8 @@
 
 	// get the HTTP method, path and body of the request
 	$method = $_SERVER['REQUEST_METHOD'];
+	echo $method;
+
 	$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 	$input = json_decode(file_get_contents('php://input'),true);
 
