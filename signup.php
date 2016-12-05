1ad6324e871118dd9cac3921c29d940a10f599cf
jakkarn <jakobnorell@gmail.com>
2016-12-05 16:07:45 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -102,7 +102,7 @@
 
 
 	<div class="center box">
-		<form action="api.php" method="post">
+		<form action="http://213.113.7.224/api.php" method="post">
 
 			<div style="top: 12%; left: 8%">
 				Username
