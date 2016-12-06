6d4e689a20723444075f206b346971bc6fa3c727
NWretborn <niclas.vretborn@gmail.com>
2016-12-06 14:19:45 +0100

    Update map.php

--- map.php
+++ map.php
@@ -6,14 +6,14 @@
   <link rel="stylesheet" type="text/css" href="libs/wifinder.css">
 </head>
 
-<script src="libs/std.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJR14TQYMP-yBtsSpULmOe0hM7bHPWasQ&libraries=places&callback=load" type="text/javascript"></script>
-<script src="libs/map.js"></script>
+
 
 <body onload="load()">
 	<input id="pac-input" class="controls" type="text" placeholder="Search Box">
 	<div id="map" style="width: 70%; height: 950px; left:30%"></div>
-
+<script src="libs/std.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJR14TQYMP-yBtsSpULmOe0hM7bHPWasQ&libraries=places&callback=load" type="text/javascript"></script>
+<script src="libs/map.js"></script>
 	<span class="logo" class="about" onclick="location.href='index.php'"> </span>
 	<span class="cache" style="background: url(img/logo_blue.png)"></span>
 </body>
