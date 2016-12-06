a88c17c14763e37a1dc01452d94a317fc14a7995
jakkarn <jakobnorell@gmail.com>
2016-12-06 14:35:08 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -13,7 +13,7 @@
 	
 	function postForm(path, formName){
 		
-		alert($("#registerform").serialize());
+		alert($(formName).serialize());
 		//var formData = JSON.stringify($("#myForm").serializeArray());
 	}
 	
@@ -113,7 +113,7 @@
 
 
 	<div class="center box">
-		<form onsubmit='postForm("registerform")' id="registerform" method='post'>
+		<form onsubmit='postForm("#registerform")' id="registerform" method='post'>
 
 			<div style="top: 12%; left: 8%">
 				Username
