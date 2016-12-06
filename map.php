ffc82b606b821c6ba7109a84859cd9b5d843d9ca
NWretborn <niclas.vretborn@gmail.com>
2016-12-06 16:40:32 +0100

    Update map.php

--- map.php
+++ map.php
@@ -11,7 +11,7 @@
 
 	<script src="libs/map.js"></script>
 	<script src="libs/std.js"></script>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJR14TQYMP-yBtsSpULmOe0hM7bHPWasQ&libraries=places&callback=load"async defer></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJR14TQYMP-yBtsSpULmOe0hM7bHPWasQ&libraries=places"async defer></script>
 
 <body onload="load()">
 	<input id="pac-input" class="controls" type="text" placeholder="Search Box">
