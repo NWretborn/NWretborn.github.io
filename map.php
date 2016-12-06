75faa60dc5ff48d7ff73867a20abd8e63003460e
NWretborn <niclas.vretborn@gmail.com>
2016-12-06 17:20:30 +0100

    Formatering

--- map.php
+++ map.php
@@ -5,18 +5,15 @@
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     	<meta charset="utf-8">
 
-  <title>WiFinder</title>
-  <link rel="stylesheet" type="text/css" href="libs/wifinder.css">
+	<title>WiFinder</title>
+	<link rel="stylesheet" type="text/css" href="libs/wifinder.css">
 </head>
-
 	<script src="libs/map.js"></script>
 	<script src="libs/std.js"></script>
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJR14TQYMP-yBtsSpULmOe0hM7bHPWasQ&libraries=places"async defer></script>
-
 <body onload="load()">
 	<input id="pac-input" class="controls" type="text" placeholder="Search Box">
 	<div id="map" style="width: 70%; height: 950px; left:30%"></div>
-	
 	
 	<span class="logo" class="about" onclick="location.href='index.php'"> </span>
 	<span class="cache" style="background: url(img/logo_blue.png)"></span>
