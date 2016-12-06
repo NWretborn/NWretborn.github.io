80f049d074d5fc8f335990070f7adf9750d121ca
NWretborn <niclas.vretborn@gmail.com>
2016-12-06 16:48:54 +0100

    Update map.js

--- libs/map.js
+++ libs/map.js
@@ -303,9 +303,9 @@ var customIcons = {
               anchor: new google.maps.Point(17, 34),
               scaledSize: new google.maps.Size(25, 25)
             } })
-   
+   map.fitBounds(bounds);
 
-    }map.fitBounds(bounds););
+    });
           
           
 
