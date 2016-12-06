6c2dcca157049d322f65e44a40d7591558814336
NWretborn <niclas.vretborn@gmail.com>
2016-12-06 15:29:45 +0100

    Update map.php

--- map.php
+++ map.php
@@ -9,15 +9,15 @@
   <link rel="stylesheet" type="text/css" href="libs/wifinder.css">
 </head>
 
-
+<script src="libs/map.js"></script>
+	<script src="libs/std.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJR14TQYMP-yBtsSpULmOe0hM7bHPWasQ&libraries=places"async defer></script>
 
 <body onload="load()">
 	<input id="pac-input" class="controls" type="text" placeholder="Search Box">
 	<div id="map" style="width: 70%; height: 950px; left:30%"></div>
 	
-	<script src="libs/map.js"></script>
-	<script src="libs/std.js"></script>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJR14TQYMP-yBtsSpULmOe0hM7bHPWasQ&libraries=places"async defer></script>
+	
 	<!--<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJR14TQYMP-yBtsSpULmOe0hM7bHPWasQ&libraries=places&callback=load" type="text/javascript"async defer></script>    style="width: 70%; height: 950px; left:30%"   -->
 	<span class="logo" class="about" onclick="location.href='index.php'"> </span>
 	<span class="cache" style="background: url(img/logo_blue.png)"></span>
