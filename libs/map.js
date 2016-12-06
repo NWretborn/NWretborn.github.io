b16450f5a9e6aee85ca16c3aef6e884e8169d78f
NWretborn <niclas.vretborn@gmail.com>
2016-12-06 15:08:32 +0100

    Update map.js

--- libs/map.js
+++ libs/map.js
@@ -8,6 +8,15 @@ var customIcons = {
   };
 
   function load() {
+      // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+    });
     var map = new google.maps.Map(document.getElementById("map"), {
       center: new google.maps.LatLng(65.618080, 22.140047),
       zoom: 10,
@@ -282,15 +291,6 @@ var customIcons = {
 
 
   function bindInfoWindow(marker, map, infoWindow, html) {
-      // Create the search box and link it to the UI element.
-    var input = document.getElementById('pac-input');
-    var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-    });
     google.maps.event.addListener(marker, 'click', function() {
       infoWindow.setContent(html);
       infoWindow.open(map, marker);         
