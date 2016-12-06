058bbd9c574552cadc71e4f00441920400641d2e
NWretborn <niclas.vretborn@gmail.com>
2016-12-06 14:34:31 +0100

    Update map.js

--- libs/map.js
+++ libs/map.js
@@ -275,14 +275,14 @@ var customIcons = {
 
 
     // Create the search box and link it to the UI element.
-    var input = document.getElementById('pac-input');
-    var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+//    var input = document.getElementById('pac-input');
+//    var searchBox = new google.maps.places.SearchBox(input);
+//    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
     // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-    });
+//   map.addListener('bounds_changed', function() {
+//    searchBox.setBounds(map.getBounds());
+//    });
   
 
 
