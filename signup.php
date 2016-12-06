4780d21dc7f27326d72b995d5d483f6f53037737
jakkarn <jakobnorell@gmail.com>
2016-12-06 14:17:22 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -13,7 +13,7 @@
 	
 	function postForm(path, formName){
 		
-		alert( serialize(document.forms("registerform") ));
+		alert(document.forms("registerform").serialize());
 		//var formData = JSON.stringify($("#myForm").serializeArray());
 	}
 	
