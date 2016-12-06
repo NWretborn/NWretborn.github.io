335bf9f2a713e4d05f006f22ae161b404ed98d37
NWretborn <niclas.vretborn@gmail.com>
2016-12-06 13:50:45 +0100

    Update map.js

--- libs/map.js
+++ libs/map.js
@@ -271,6 +271,18 @@ var customIcons = {
     });
   }
 
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+    });
+
+
   function bindInfoWindow(marker, map, infoWindow, html) {
     google.maps.event.addListener(marker, 'click', function() {
       infoWindow.setContent(html);
