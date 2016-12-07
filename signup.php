05ef3f02ca9a0949ba854a2ef2bd358065957132
jakkarn <jakobnorell@gmail.com>
2016-12-07 14:56:37 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -28,8 +28,8 @@
 		//for(var i in showData){
 		//	arr.push(showData[i]);
 		//}
-		alert($(formName) + "\n" + origData + "\n" + formData);
-		alert(formData[0] + "\n" + formData[0][0]);
+		//alert($(formName) + "\n" + origData + "\n" + formData);
+		alert(formData + "\n\n" + formData[0] + "\n" + formData[0][0]);
 		$.ajax({
 			type: "POST",
 			url: "./"+path,			// "/" would refer to root, it seems
