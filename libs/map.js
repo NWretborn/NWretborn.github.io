1eecb63440bcf34a7e00cf44ed4a636ba345077a
NWretborn <niclas.vretborn@gmail.com>
2016-12-07 15:46:02 +0100

    Update map.js

--- libs/map.js
+++ libs/map.js
@@ -10,7 +10,7 @@ var customIcons = {
 function load() {
      
 	var map = new google.maps.Map(document.getElementById("map"), {
-	center: new google.maps.LatLng(65.618080, 22.140047),
+	center: new google.maps.LatLng(65.618080, 122.140047),
 	zoom: 10,
 	styles:
       [
