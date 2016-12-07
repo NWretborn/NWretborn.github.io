dbd3fe175de8f891d90442e845ec63a5690acd94
jakkarn <jakobnorell@gmail.com>
2016-12-07 14:48:39 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -28,7 +28,7 @@
 		//for(var i in showData){
 		//	arr.push(showData[i]);
 		//}
-		alert($(formname) + "\n" + origData + "\n" + formData);
+		alert($(formName) + "\n" + origData + "\n" + formData);
 		
 		$.ajax({
 			type: "POST",
