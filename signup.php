2f2a8677814f1a407429ad0bac4b67fa814df747
jakkarn <jakobnorell@gmail.com>
2016-12-07 13:25:36 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -147,7 +147,7 @@
 
 
 	<div class="center box">
-		<form onsubmit='postForm("http://213.113.7.224/api.php", "#registerform")' id="registerform">
+		<form onsubmit='postForm("api.php", "#registerform")' id="registerform">
 
 			<div style="top: 12%; left: 8%">
 				Username
