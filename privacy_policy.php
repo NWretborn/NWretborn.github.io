c2dd2b1396b1f7d99e877ef209a1d871357f998c
jakkarn <jakobnorell@gmail.com>
2016-12-08 13:56:16 +0100

    Update privacy_policy.php

--- privacy_policy.php
+++ privacy_policy.php
@@ -12,9 +12,7 @@
 
 	<section id="privacy-information-we-collect">
 		<h3>What information do we collect?</h3>
-		<p>We collect information from you when you register on our site, fill out a form or when you explicitly upload data to our website.</p>
-		<p>When ordering or registering on our site, as appropriate, you may be asked to enter your: name, e-mail address, mailing address, phone number, credit card information or social security number. You may, however, visit our site anonymously.
-		Google, as a third party vendor, uses cookies to serve ads on your site. Google's use of the DART cookie enables it to serve ads to your users based on their visit to your sites and other sites on the Internet. Users may opt out of the use of the DART cookie by visiting the Google ad and content network privacy policy.</p>
+		<p>We collect information from you when you register on our site, fill out a form or when you explicitly upload data to our website. We might collect and store data without you knowing about it.</p>
 	</section>
 
 	<section id="privacy-information-use">
@@ -30,22 +28,22 @@
 	<section id="privacy-cookies">
 		<h3>Do we use cookies?</h3>
 		<p>Yes. Cookies are small files that a site or its service provider transfers to your computers hard drive through your Web browser (if you allow them) that enable the sites or service providers systems to recognize your browser and capture and remember certain information</p>
-		<p>If you prefer, you can choose to have your computer warn you each time a cookie is being sent, or you can choose to turn off all cookies via your browser settings. Like most websites, if you turn your cookies off, some of our services may not function properly. However, you can still place orders over the telephone or by contacting customer service.</p>
+		<p>If you prefer, you can choose to have your computer warn you each time a cookie is being sent, or you can choose to turn off all cookies via your browser settings. Like most websites, if you turn your cookies off, some of our services may not function properly.</p>
 	</section>
 
 	<section id="privacy-disclose-third-party">
 		<h3>Do we disclose any information to outside parties?</h3>
-		<p>We do not sell, trade, or otherwise transfer to outside parties your personally identifiable information. This does not include trusted third parties who assist us in operating our website, conducting our business, or servicing you, so long as those parties agree to keep this information confidential. We may also release your information when we believe release is appropriate to comply with the law, enforce our site policies, or protect ours or others rights, property, or safety. However, non-personally identifiable visitor information may be provided to other parties for marketing, advertising, or other uses.</p>
+		<p>We do not sell or trade any of the information uploaded to our site. We may however use the information uploaded to our website in third-party applications.</p>
 	</section>
 
 	<section id="privacy-third-party-links">
 		<h3>Third party links</h3>
-		<p>Occasionally, at our discretion, we may include or offer third party products or services on our website. These third party sites have separate and independent privacy policies. We therefore have no responsibility or liability for the content and activities of these linked sites. Nonetheless, we seek to protect the integrity of our site and welcome any feedback about these sites.</p>
+		<p>Occasionally, at our discretion, we may include or offer third party products or services on our website. These third party sites have separate and independent privacy policies. We therefore have no responsibility or liability for the content and activities of these linked sites.</p>
 	</section>
 
 	<section id="privacy-online">
 		<h3>Online Privacy Policy Only</h3>
-		<p>This online privacy policy applies only to information collected through our website and not to information collected offline.</p>
+		<p>This online privacy policy applies only to information collected through our website online.</p>
 	</section>
 
 	<section id="privacy-consent">
