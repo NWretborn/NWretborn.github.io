e4fda9f2b2ebbe4d2de910adb31e75b0355432d0
NWretborn <niclas.vretborn@gmail.com>
2016-12-08 16:37:46 +0100

    Update map2.php

--- map2.php
+++ map2.php
@@ -50,7 +50,7 @@ function openTab(evt, tabName) {
   for (i = 0; i < x.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " w3-red";
 }
 </script>
