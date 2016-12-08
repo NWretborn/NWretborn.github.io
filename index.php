2d52517f40398b70dd1b421a81431e09fd14de51
jakkarn <jakobnorell@gmail.com>
2016-12-08 15:28:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,6 +19,14 @@
 		console.log('Image URL: ' + profile.getImageUrl());
 		console.log('Email: ' + profile.getEmail());
 	}
+
+	function signOut() {
+		var auth2 = gapi.auth2.getAuthInstance();
+		auth2.signOut().then(function () {
+			console.log('User signed out.');
+		});
+	}
+
 </script>
 	
 	
@@ -49,6 +57,7 @@
 				<input 	type="submit" value="Continue as guest" name="register" 
 						style="top:calc(var(--FONT_HEIGHT)*6.5); left:35%; width:35%; min-width:calc(var(--FONT_HEIGHT)*15/2);">
 			</form>
+			<a href="#" onclick="signOut();">Sign out</a>
 		</div>
 		
 
