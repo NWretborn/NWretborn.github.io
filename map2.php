af6ee29305033d43bf0d4fb2dccf207e2587bb87
NWretborn <niclas.vretborn@gmail.com>
2016-12-08 15:35:32 +0100

    Update map2.php

--- map2.php
+++ map2.php
@@ -19,38 +19,10 @@
 	<div class="box left">
 		<div class="text_box">
 
-			<div class="left text" style="top:calc(var(--FONT_HEIGHT)*1.4);">
-				Username
-			</div>
-			<input type="text" name="username"
-			style="top:calc(var(--FONT_HEIGHT)*1.4); width:65%; right:0.2vh;">
-
-			<div class="left text" style="top:calc(var(--FONT_HEIGHT)*3.7);">
-				Password
-			</div>
-			<input type="password" name="password" 
-			style="top:calc(var(--FONT_HEIGHT)*3.7); width:65%; right:0.2vh;">
 			
-			<form action="map.php" method="get">
-				<input type="submit" value="Log in" name="register" 
-				style="top:calc(var(--FONT_HEIGHT)*6.5); width:20%; right:0.2vh; ">
-			</form>
-
-			<form action="map.php" method="get">
-				<input 	type="submit" value="Continue as guest" name="register" 
-						style="top:calc(var(--FONT_HEIGHT)*6.5); left:35%; width:35%; min-width:calc(var(--FONT_HEIGHT)*15/2);">
-			</form>
-			<a href="#" onclick="signOut();">Sign out</a>
-		</div>
-		
-
-		<!--<form action="signup.php" method="get">-->
-		<div onclick="location.href='signup.php'">
-			<input type="submit" value ="Sign up" name="register" class="hCenter centerpoint" style="position:absolute; top:98%;" >
-		</div>
 		<!--</form>-->
 </div> 
-	
+</div> 
 	
 	
 	
