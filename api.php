8ef1b74b720d77190a4838e2ef6cb3020ac1b565
jakkarn <jakobnorell@gmail.com>
2016-12-08 17:08:47 +0100

    apiUpdate

--- api.php
+++ api.php
@@ -5,14 +5,14 @@
 
 	// get the HTTP method, path and body of the request
 	$method = $_SERVER['REQUEST_METHOD'];
-	
+
 	error_log("api.php received ".$method." request\n", 3, "./scrap.log");
 
 	$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 	//echo "<br/>php://input: ".file_get_contents('php://input');
 	$input = json_decode(file_get_contents('php://input'),true);
-	
-	error_log("input: ".file_get_contents('php://input')."\n", 3, "./scrap.log");
+
+	//error_log("input: ".file_get_contents('php://input')."\n", 3, "./scrap.log");
 
 	// connect to the mysql database
 	$link = mysqli_connect('localhost', $username, $password, $database);
@@ -20,7 +20,7 @@
 
 	// retrieve the table and key from the path
 	$table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
-	
+
 	$key = array_shift($request)+0;
 
 	// escape the columns and values from the input object
@@ -29,32 +29,30 @@
 		if ($value===null) return null;
 		return mysqli_real_escape_string($link,(string)$value);
 	},array_values($input));
-	
+
 	// build the SET part of the SQL command
-	error_log("received following data:\n", 3, "./scrap.log");
+	//error_log("received following data:\n", 3, "./scrap.log");
 	$set = '';
 	for ($i=0;$i<count($columns);$i++) {
 		$set.=($i>0?',':'').'`'.$columns[$i].'`=';
 		$set.=($values[$i]===null?'NULL':'"'.$values[$i].'"');
-		error_log("\tcolumn: ".$columns[$i], 3, "./scrap.log");
-		error_log("\tvalue: ".$values[$i]."\n", 3, "./scrap.log");
 	}
-	
+
 	// create SQL based on HTTP method
 	switch ($method) {
 		case 'GET':
-			$sql = "select * from `$table`".($key?" WHERE id=$key":''); 
+			$sql = "select * from `$table`".($key?" WHERE id=$key":'');
 			break;
 		case 'PUT':
-			$sql = "update `$table` set $set where id=$key"; 
+			$sql = "update `$table` set $set where id=$key";
 			break;
 		case 'POST':
 			//$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-			error_log($set, 3, "./scrap.log");
+			error_log($set."\n", 3, "./scrap.log");
 			$sql = "insert into `$table` set $set";
 			break;
 		case 'DELETE':
-			$sql = "delete from `$table` where id=$key"; 
+			$sql = "delete from `$table` where id=$key";
 			break;
 	}
 
@@ -76,16 +74,16 @@
 	if ($method == 'GET') {
 		if (!$key) echo '[';
 		for ($i=0;$i<mysqli_num_rows($result);$i++) {
-			error_log($i>0?',':'').json_encode(mysqli_fetch_object($result), 3, "./scrap.log");
+			//error_log($i>0?',':'').json_encode(mysqli_fetch_object($result), 3, "./scrap.log");
 			echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
 		}
 		if (!$key)
 			echo ']';
 	} elseif ($method == 'POST') {
-		error_log('{ "success":true, "data":[ { "id":'.mysqli_insert_id($link).' }]}', 3, "./scrap.log");
+		//error_log('{ "success":true, "data":[ { "id":'.mysqli_insert_id($link).' }]}', 3, "./scrap.log");
 		echo '{ "success":true, "data":[ { "id":'.mysqli_insert_id($link).' }]}';
 	} else {
-		error_log("affected_rows:".mysqli_affected_rows($link), 3, "./scrap.log");
+		//error_log("affected_rows:".mysqli_affected_rows($link), 3, "./scrap.log");
 		echo mysqli_affected_rows($link);
 	}
 
