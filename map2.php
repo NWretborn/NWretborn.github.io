a0ba9168443699e9c1a61b43096ba6e4cc1cbd28
NWretborn <niclas.vretborn@gmail.com>
2016-12-08 15:45:20 +0100

    Update map2.php

--- map2.php
+++ map2.php
@@ -18,7 +18,43 @@
 	
 	<div class="box left" style="width: 30%; height: 75%;">
 		<div class="text_box">
+	<div class="w3-container">
+  <h2>Tabs</h2>
+  <p>Tabs are perfect for single page web applications, or for web pages capable of displaying different subjects. Click on the links below.</p>
+</div>
 
+<ul class="w3-navbar w3-black">
+  <li><a href="javascript:void(0)" onclick="openCity('London')">London</a></li>
+  <li><a href="javascript:void(0)" onclick="openCity('Paris')">Paris</a></li>
+  <li><a href="javascript:void(0)" onclick="openCity('Tokyo')">Tokyo</a></li>
+</ul>
+
+<div id="London" class="w3-container city">
+  <h2>London</h2>
+  <p>London is the capital city of England.</p>
+</div>
+
+<div id="Paris" class="w3-container city">
+  <h2>Paris</h2>
+  <p>Paris is the capital of France.</p> 
+</div>
+
+<div id="Tokyo" class="w3-container city">
+  <h2>Tokyo</h2>
+  <p>Tokyo is the capital of Japan.</p>
+</div>
+
+<script>
+openCity("London")
+function openCity(cityName) {
+    var i;
+    var x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    document.getElementById(cityName).style.display = "block";  
+}
+</script>
 			
 		<!--</form>-->
 </div> 
