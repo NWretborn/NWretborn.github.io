6d974f2568ec07c8418ad230bf9b6f2f646a650c
NWretborn <niclas.vretborn@gmail.com>
2016-12-09 16:57:27 +0100

    Update map.js

--- libs/map.js
+++ libs/map.js
@@ -373,8 +373,8 @@ function geolocate() {
 			firstSet = true;
   		}
 		var locArr = String(location);
-		var lat = String(locArr.slice(1,18));
-		var lon = String(locArr.slice(21,38));
+		var lat = String(locArr.slice(1,17));
+		var lon = String(locArr.slice(20,37));
 		document.getElementById("lat").innerHTML=lat;
 		document.getElementById("lon").innerHTML=lon;
 	}
