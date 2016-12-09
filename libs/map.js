87800cbfe5440218e1daf987dc2e4c4ad6b673c5
NWretborn <niclas.vretborn@gmail.com>
2016-12-09 15:19:52 +0100

    Update map.js

--- libs/map.js
+++ libs/map.js
@@ -358,11 +358,11 @@ function geolocate() {
 			
 	function placeMarker(location) {
 		var markers = [];
-    		var markers = [new google.maps.Marker({
+    		var markers = new google.maps.Marker({
         	position: location, //The variable 'location' contains the lat and lng values needed for the db
         	map: map,
 		icon: icon.icon
-    		});];
+    		});
 		document.getElementById("location").innerHTML=location;
 	}
 			
