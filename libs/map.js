a7dad93ef14072e8de7ad89105b28c2def0f8513
NWretborn <niclas.vretborn@gmail.com>
2016-12-10 04:37:26 +0100

    Update map.js

--- libs/map.js
+++ libs/map.js
@@ -457,29 +457,28 @@ function geolocate() {
 }
 
 
-
 	function bindInfoWindow(marker, map, infoWindow, html) {
 		google.maps.event.addListener(marker, 'click', function() {
 		infoWindow.setContent(html);
 		infoWindow.open(map, marker);
-		var name = marker.getAttribute("name");
-		var address = marker.getAttribute("address");
-		var type = marker.getAttribute("type");
-		var quality = marker.getAttribute("quality");
-		var karma = marker.getAttribute("karma");
-		var user = marker.getAttribute("user");
-		var htmlname = String(name);
-		var htmladdress = String(address);
-		var htmltype = String(type);
-		var htmlquality = String(quality);
-		var htmlkarma = String(karma);
-		var htmluser = String(user);
-		document.getElementById("htmlname").innerHTML=htmlname;
-		document.getElementById("htmladdress").innerHTML=htmladdress;
-		document.getElementById("htmltype").innerHTML=htmltype;
-		document.getElementById("htmlquality").innerHTML=htmlquality;
-		document.getElementById("htmlkarma").innerHTML=htmlkarma;
-		document.getElementById("htmluser").innerHTML=htmluser; 
+		//var name = marker.getAttribute("name");
+		//var address = marker.getAttribute("address");
+		//var type = marker.getAttribute("type");
+		//var quality = marker.getAttribute("quality");
+		//var karma = marker.getAttribute("karma");
+		//var user = marker.getAttribute("user");
+		//var htmlname = String(name);
+		//var htmladdress = String(address);
+		//var htmltype = String(type);
+		//var htmlquality = String(quality);
+		//var htmlkarma = String(karma);
+		//var htmluser = String(user);
+		//document.getElementById("htmlname").innerHTML=htmlname;
+		//document.getElementById("htmladdress").innerHTML=htmladdress;
+		//document.getElementById("htmltype").innerHTML=htmltype;
+		//document.getElementById("htmlquality").innerHTML=htmlquality;
+		//document.getElementById("htmlkarma").innerHTML=htmlkarma;
+		document.getElementById("htmluser").innerHTML=marker; 
 	});
 }
 
