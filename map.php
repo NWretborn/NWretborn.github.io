a9872d27e62b0e21df376fb8c4b1376bab7dece5
NWretborn <niclas.vretborn@gmail.com>
2016-12-11 18:11:26 +0100

    Update map.php

--- map.php
+++ map.php
@@ -130,7 +130,7 @@
 				<input value="yes" type="text" name="type" font="monospace" class="tb5">
 			</div>
 		  	<br/>
-		  	<div style="font-size: 20px; top: 114%"> 
+		  	<div style="font-size: 20px; top: 64%"> 
 		   		<form id="Upload" action="<?php echo $uploadHandler ?>" enctype="multipart/form-data" method="post"> 
         				<p> 
             					<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $max_file_size ?>"> 
