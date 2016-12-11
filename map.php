9def8550774e1cbd62affed8f47a2fd4f2cd721f
NWretborn <niclas.vretborn@gmail.com>
2016-12-12 00:59:12 +0100

    Update map.php

--- map.php
+++ map.php
@@ -165,7 +165,30 @@ $max_file_size = 30000; // size in bytes
 
 </div>
 		  
-		  <form id="Upload" action="<?php echo $uploadHandler ?>" enctype="multipart/form-data" method="post"> 
+		  
+		  <form id="Upload" action="<?php echo $uploadHandler ?>" enctype="multipart/form-data" method="post">
+	
+		<h1>
+			Upload form
+		</h1>
+		
+		<p>
+			<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $max_file_size ?>">
+		</p>
+		
+		<p>
+			<label for="file">File to upload:</label>
+			<input id="file" type="file" name="file">
+		</p>
+				
+		<p>
+			<label for="submit">Press to...</label>
+			<input id="submit" type="submit" name="submit" value="Upload me!">
+		</p>
+	
+	</form>
+		  
+		<!--  <form id="Upload" action="<?php echo $uploadHandler ?>" enctype="multipart/form-data" method="post"> 
 			<br/>
 		  	<div style="font-size: 20px; top: 64%"> 
 			Picture(optional)<br/>
@@ -173,7 +196,7 @@ $max_file_size = 30000; // size in bytes
    			
 			</div> 			  
 		  </form>
-
+		-->
 			
 </form>
 	  
