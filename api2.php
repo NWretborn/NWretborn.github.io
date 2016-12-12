03e9d254ab16cdb4453f9d7f51a0eb2c61f26844
jakkarn <jakobnorell@gmail.com>
2016-12-12 16:41:51 +0100

    Update api2.php

--- api2.php
+++ api2.php
@@ -31,8 +31,7 @@
 
 <?php
 
-	ini_set("log_errors", 1);
-	ini_set("error_log", "./error.log");
+	ini_set('display_errors', 3, "./error.log");
 
 	// error-message written to a scrap log in same folder
 	function errlog($message){
