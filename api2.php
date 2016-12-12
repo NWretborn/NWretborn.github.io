bf1cea70a05e6b5aa5655fd840323afbb78f639a
jakkarn <jakobnorell@gmail.com>
2016-12-12 18:06:33 +0100

    Update api2.php

--- api2.php
+++ api2.php
@@ -1,3 +1,138 @@
 <?php
 	session_start();
 ?>
+
+<?php
+	#ini_set("log_errors", 1);
+	#ini_set("error_log", "/tmp/php-error.log");
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+
+	// error-message written to a scrap log in same folder
+	function errlog($message){
+		error_log($message."\n", 3, "./scrap2.log");
+	}
+
+	function badcall($isbad, $message){
+		if($isbad)
+			errlog($message);
+			http_response_code(404);
+			die($message);
+	}
+
+	function hashpass($password){
+		return password_hash($password, PASSWORD_DEFAULT);
+	}
+	
+	function verifypass($password){
+		return password_verify($password, $hashed_password);
+	}
+
+	errlog("STARTLOG");
+	errlog("--------------------------------------------------");
+	require("phpsqlajax_dbinfo.php");
+
+	// get the HTTP method, path and body of the request
+	$method = $_SERVER['REQUEST_METHOD'];
+	errlog($_SERVER['PHP_SELF']." received ".$method." request");
+	$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+	$input = json_decode(file_get_contents('php://input'),true);
+
+	// connect to the mysql database
+	$link = mysqli_connect('localhost', $username, $password, $database);
+	mysqli_set_charset($link,'utf8');
+
+	// retrieve table and key from the path remove from variable $request
+	$apicall = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
+	$key = array_shift($request)+0;
+
+	switch($apicall){
+		case 'adduser':
+			$table='user';
+			if($input['password'])
+				$input['password'] = hashpass($input['password']);
+			badcall($method!='POST', "use POST for adduser"); //NOT SURE IF BADCALL IS GOOD METHOD
+			break;
+		case 'addwifi':
+			$table='markers';
+			badcall($method!='POST', "use POST for addwifi");
+			break;
+		case 'login':
+			$table='user';
+			badcall($method!='GET', "use GET for login");
+			break;
+		default:
+			badcall(True, "not an api-function");
+			break;
+	}
+
+	// escape the columns and values from the input object
+	$columns = preg_replace('/[^a-z0-9_]+/i','',array_keys($input));
+	$values = array_map(function ($value) use ($link) {
+		if ($value===null) return null;
+		return mysqli_real_escape_string($link,(string)$value);
+	},array_values($input));
+	
+	// build the SET part of the SQL command
+	$set = '';
+	for ($i=0;$i<count($columns);$i++) {
+		$set.=($i>0?',':'').'`'.$columns[$i].'`=';
+		$set.=($values[$i]===null?'NULL':'"'.$values[$i].'"');
+	}
+	
+	// create SQL based on HTTP method
+	switch ($method) {
+		case 'GET':
+			$sql = "select * from `$table`".($key?" WHERE id=$key":'');
+			break;
+		case 'PUT':
+			$sql = "update `$table` set $set where id=$key";
+			break;
+		case 'POST':
+			$sql = "insert into `$table` set $set";
+			break;
+		case 'DELETE':
+			$sql = "delete from `$table` where id=$key";
+			break;
+	}
+	errlog("sql {".$sql."} sent");
+	
+	// excecute SQL statement
+	$result = mysqli_query($link,$sql);
+	$errors = mysqli_error($link);
+	if($errors){
+		errlog("SQL ERROR: ".$errors);
+	}
+	
+	// die if SQL statement failed
+	if (!$result) {
+		errlog("sql no result");
+		http_response_code(404);
+		die(mysqli_error());
+	}
+	else{
+		http_response_code(200);
+	}
+	
+	// print results, insert id or affected row count
+	if ($method == 'GET') {
+		if (!$key) echo '[';
+		for ($i=0;$i<mysqli_num_rows($result);$i++) {
+			errlog(($i>0?',':'').json_encode(mysqli_fetch_object($result)));
+			echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
+		}
+		if (!$key)
+			echo ']';
+	} elseif ($method == 'POST') {
+		echo '{ "success":true, "data":[ { "id":'.mysqli_insert_id($link).' }]}';
+		errlog('{ "success":true, "data":[ { "id":'.mysqli_insert_id($link).' }]}');
+	} else {
+		errlog( mysqli_affected_rows($link) );
+		echo mysqli_affected_rows($link);
+	}
+	
+	// close mysql connection
+	mysqli_close($link);
+	errlog("--------------------------------------------------");
+	errlog("ENDLOG");
+?>
