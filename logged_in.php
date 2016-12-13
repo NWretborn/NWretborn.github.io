29b03832d8baf08c2c9655c4fee98018ca84e6f0
jakkarn <jakobnorell@gmail.com>
2016-12-13 16:42:46 +0100

    Update logged_in.php

--- logged_in.php
+++ logged_in.php
@@ -1,3 +1,7 @@
 <div onclick="location.href='map.php'">
 	<input type="submit" value ="To the map!" name="register" class="center" style="position:fixed;" >
 </div>
+
+<div onclick="logout();">
+	<input type="submit" value ="log out" name="register" style="position:fixed; right:1vw; top:1vh;" >
+</div>
