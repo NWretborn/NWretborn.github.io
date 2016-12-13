4104fe92b28c6b09fde4b87ea321f7e378ae88be
jakkarn <jakobnorell@gmail.com>
2016-12-13 16:26:06 +0100

    Update logged_in.php

--- logged_in.php
+++ logged_in.php
@@ -1,5 +1,3 @@
-<span id="center" style="position:fixed">
-	<div id="tothemap center" onclick="location.href='map.php'"> 
-		TO THE MAP!
-	</a>
-</span>
+<div onclick="location.href='map.php'">
+	<input type="submit" value ="To the map!" name="register" class="center" style="position:fixed;" >
+</div>
