0eabe4a9e96c78d7e06cd2c6f18fd555f9bfea0d
jakkarn <jakobnorell@gmail.com>
2016-12-13 03:49:48 +0100

    Update map.php

--- map.php
+++ map.php
@@ -223,7 +223,7 @@ function openTab(evt, tabName) {
 	<span id="logo" class="about" onclick="location.href='index.php'"> </span>
 	<span class="cache" style="background: url(img/logo_blue.png)"></span>
 	<input id="pac-input" class="controls" type="text" placeholder="Search Box">
-	<div id="map" style="width: 70%; height: 1400px; left:30%"></div>		<?php #changed height 950px->100% ?>
+	<div id="map" style="width: 70%; height: 100vh; left:30%"></div>		<?php #changed height 950px->100% ?>
 	
 	<span class="logo" class="about" onclick="location.href='index.php'"> </span>
 	<span class="cache" style="background: url(img/logo_blue.png)"></span>
