2dbb2c6939747d4feebd0a411ea1637940512ea0
NWretborn <niclas.vretborn@gmail.com>
2016-12-20 16:49:21 +0100

    Update map.php

--- map.php
+++ map.php
@@ -217,7 +217,7 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " w3-cyan";
 	alert(holder);
-	 if(holder[0] = 'a'){
+	 if(holder === 'addNetwork'){
   addnetboolean = true;
 		 alert('add');
   }
