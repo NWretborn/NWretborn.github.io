dc83063f8343faa9a2917a20397ec3d7807b051c
NWretborn <niclas.vretborn@gmail.com>
2016-12-21 02:06:30 +0100

    Update map.php

--- map.php
+++ map.php
@@ -208,8 +208,7 @@ function openTab(evt, tabName) {
   var i, x, tablinks;
   x = document.getElementsByClassName("tab");
   for (i = 0; i < x.length; i++) {
-      //x[i].style.display = "none";
-	  tablinks[i].className = tablinks[i].className.replace(" w3-cyan", "");
+      x[i].style.display = "none";
   }
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < x.length; i++) {
