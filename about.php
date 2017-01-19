18e35e226d5b9f88a0cc20a83d8231ff2c5d5e96
NWretborn <niclas.vretborn@gmail.com>
2017-01-19 14:53:16 +0100

    Update about.php

--- about.php
+++ about.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-	<span class="about_box center" style="height: 80%;">
+	<span class="about_box center" style="height: 60%;">
 		<span class="center text_box" style="padding-right:2vw;">
 			<span style="color: #29afc4;">WiFinder</span> is a user-driven website for finding free wifi networks(coffee shops, libraries etc) with the help of google maps.<br/>
 			Networks are rated by users, submitted by users and the top rated review displayed first when viewing a network.<br/>
