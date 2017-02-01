2156e24b925f398462b37071f71e532931b5e76d
NWretborn <niclas.vretborn@gmail.com>
2017-02-01 15:23:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,12 +19,7 @@
 
 <script>
 
-	function signOut() {
-		var auth2 = gapi.auth2.getAuthInstance();
-		auth2.signOut().then(function () {
-			console.log('User signed out.');
-		});
-	}
+
 	
 	// post the submission form to API via ajax in json format
 	function loginForm(){
