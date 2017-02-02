13412ecfcf1cff8de1416c2dcb0ee425cf560377
NWretborn <niclas.vretborn@gmail.com>
2017-02-02 14:56:52 +0100

    Update map.php

--- map.php
+++ map.php
@@ -151,7 +151,7 @@ $max_file_size = 2000000; // size in bytes
 </div>
 		   <form id="deleteform" enctype="multipart/form-data" onsubmit='deleteForm("../api.php/deletewifi", "#deleteform")' method="post">
 			     <div  style="font-size: 20px; top: 92%">
-				<p type ="hidden" style="font-size: 20px; color: #10454e;" id="htmlpicurldel" value="htmlpicurldel" href = test.html></p>
+				<input type ="hidden" style="font-size: 20px; color: #10454e;" id="htmlpicurldel" value="htmlpicurldel" href = test.html>
 </div>
 		  <div style="font-size: 20px; top: 114%">
 <input style="color: #29afc4; margin-top: 10px;" id="submit" type="submit" name="deletewifi" value="Delete WiFi" class="tb5"> 
