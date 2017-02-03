a845c33cecc1dfa2a2eaa75e62b0082510af31f5
NWretborn <niclas.vretborn@gmail.com>
2017-02-03 16:16:44 +0100

    Update map.php

--- map.php
+++ map.php
@@ -155,7 +155,7 @@ $max_file_size = 2000000; // size in bytes
 				<input type ="hidden" style="font-size: 20px; color: #10454e;" name="picurl" id="htmlpicurldel" href = test.html>
 </div>
 			   <div  style="font-size: 20px; top: 92%">
-				<input type ="hidden" style="font-size: 20px; color: #10454e;" name="user" id="htmluser" href = test.html>
+				<input type ="hidden" style="font-size: 20px; color: #10454e;" name="user" id="htmluserdel" href = test.html>
 </div>
 		  <div style="font-size: 20px; top: 114%">
 <input style="color: #29afc4; margin-top: 10px;" id="submit" type="submit" name="deletewifi" value="Delete WiFi" class="tb5"> 
