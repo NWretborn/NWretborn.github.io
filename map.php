16f2660e155c7855ba4fbc41a62e501f661b015f
NWretborn <niclas.vretborn@gmail.com>
2017-02-03 16:39:21 +0100

    Update map.php

--- map.php
+++ map.php
@@ -96,7 +96,7 @@ $max_file_size = 2000000; // size in bytes
 	// post the submission form to API via ajax in json format
 	function deleteForm(path, formID){
    		document.getElementById("picurl").value = picurl;
-		//document.getElementById("user").value = user;
+		document.getElementById("user").value = user;
 		var jsonOUT =$(formID).serializeObject();
 			var jsonSTR = JSON.stringify(jsonOUT);
 			$.ajax({
@@ -154,9 +154,9 @@ $max_file_size = 2000000; // size in bytes
 			     <div  style="font-size: 20px; top: 92%">
 				<input type ="hidden" style="font-size: 20px; color: #10454e;" name="picurl" id="htmlpicurldel" href = test.html>
 </div>
-			<!--   <div  style="font-size: 20px; top: 92%">
+			   <div  style="font-size: 20px; top: 92%">
 				<input type ="hidden" style="font-size: 20px; color: #10454e;" name="user" id="htmluserdel" href = test.html>
-</div>-->
+</div>
 		  <div style="font-size: 20px; top: 114%">
 <input style="color: #29afc4; margin-top: 10px;" id="submit" type="submit" name="deletewifi" value="Delete WiFi" class="tb5"> 
 					
