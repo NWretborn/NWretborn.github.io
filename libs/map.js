e4fc6f25041c39fb0dba742d00a5d51f081c58aa
NWretborn <niclas.vretborn@gmail.com>
2017-08-03 00:52:16 +0200

    Update map.js

--- libs/map.js
+++ libs/map.js
@@ -469,7 +469,7 @@ function geolocate() {
 		document.getElementById("htmlquality").innerHTML=String("Review: " + htmldat[3]);
 		document.getElementById("htmluser").innerHTML=String("Submitted by: " + htmldat[4]); 
 		document.getElementById("htmltype").innerHTML=String("Customer only: " + htmldat[5]);
-		document.getElementById("htmlpicurl").src=String("http://213.113.2.100/uploaded_files/"+htmldat[6]);
+		document.getElementById("htmlpicurl").src=String("http://213.113.7.240/uploaded_files/"+htmldat[6]);
 		document.getElementById("htmluserdel").value=String(htmldat[4]);
 		document.getElementById("htmlpicurldel").value=String(htmldat[6]);
 		infoWindow.setContent(htmldat[0]);
