3904154b4d015e245cdf598faf90f860adaaaf0d
NWretborn <niclas.vretborn@gmail.com>
2017-10-23 14:45:54 +0200

    Update signed_in.php

--- signed_in.php
+++ signed_in.php
@@ -1,5 +1,9 @@
 <?php
-	if(isset($_SESSION['username']))
+	if(isset($_SESSION['username'])){
 		session_start();
-	
+	}
+	else{
+						$message = "wrong credentials";
+				echo "<script type='text/javascript'>alert('$message');</script>";
+	}
 ?>
