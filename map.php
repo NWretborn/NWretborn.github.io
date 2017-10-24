14173885429acda91eaf41398b223cc479d9f6f1
NWretborn <niclas.vretborn@gmail.com>
2017-10-24 15:54:20 +0200

    Update map.php

--- map.php
+++ map.php
@@ -73,7 +73,7 @@ $max_file_size = 2000000; // size in bytes
    		document.getElementById("picurl").value = picurl;
 		var jsonOUT =$(formID).serializeObject();
 		var type = jsonOUT['type'];
-		if(name == '' || address == ''){
+		if(name.length == 0 || address.length == 0){
 -			alert("Please fill out all the information before submitting");
 -		}
 		else{
