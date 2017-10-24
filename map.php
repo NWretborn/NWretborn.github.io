893624fd282760f4a1611075bf7ab783df780cad
NWretborn <niclas.vretborn@gmail.com>
2017-10-24 15:58:39 +0200

    Update map.php

--- map.php
+++ map.php
@@ -74,7 +74,10 @@ $max_file_size = 2000000; // size in bytes
 		var jsonOUT =$(formID).serializeObject();
 		var name = jsonOUT['name'];
 		var address = jsonOUT['address'];
-		
+		if(name == '' || address == ''){
+-			alert("Please fill out all the information before submitting");
+-		}
+		else{
 			var jsonSTR = JSON.stringify(jsonOUT);
 			$.ajax({
 				type: "POST",
@@ -88,7 +91,7 @@ $max_file_size = 2000000; // size in bytes
 			window.alert("Network added!");
 			window.location = "/map.php";
 		
-		
+		}
 		
 		
 		
