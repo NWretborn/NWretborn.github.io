50df8a372fff66d57fa5ee2e0b7b3eb2e63f7a6d
NWretborn <niclas.vretborn@gmail.com>
2017-10-28 00:15:38 +0200

    Create login_box_error.php

---
+++ login_box_error.php
@@ -0,0 +1,43 @@
+<div class="box center">
+	<div class="text_box">
+	<div style="color: #29afc4; font-size: 16px">Wrong credentials</div>
+		<div class="left text" style="top:calc(var(--FONT_HEIGHT)*1.4);">
+			Username
+		</div>
+		<input id="username" type="text" name="username"
+		style="top:calc(var(--FONT_HEIGHT)*1.4); width:65%; right:0.2vh;">
+
+		<div class="left text" style="top:calc(var(--FONT_HEIGHT)*3.7);">
+			Password
+		</div>
+		<input id="password" type="password" name="password" 
+		style="top:calc(var(--FONT_HEIGHT)*3.7); width:65%; right:0.2vh;">
+
+		<form onsubmit="loginForm(); return false;" method="get">
+			<input type="submit" value="Log in" name="register" id="login"
+			style="top:calc(var(--FONT_HEIGHT)*6.5); width:20%; right:0.2vh; ">
+			
+		</form>
+		<!--	GOOGLE SIGNOUT
+		<a href="#" onclick="signOut();">Sign out</a>
+		-->
+	</div>
+	
+	
+	<!--<form action="signup.php" method="get">-->
+	<div onclick="location.href='signup.php'">
+		<input type="submit" value ="Sign up" name="register" class="hCenter centerpoint" style="position:absolute; top:98%;" >
+	</div>
+	<!--</form>-->
+</div>	
+<html>
+<script>
+	document.getElementById("password")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+        document.getElementById("login").click();
+    }
+});
+</script>
+</html>
