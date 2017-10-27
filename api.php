4c9028c23d9deb37b4d037e5b393346dbdab4425
NWretborn <niclas.vretborn@gmail.com>
2017-10-27 17:15:59 +0200

    Update api.php

--- api.php
+++ api.php
@@ -88,7 +88,7 @@
 			$table='user';
 			$curuser =  $input['name'];
 			$test = "SELECT name FROM 'user' WHERE name=mysql_real_escape_string($curuser)";
-			$usertester = mysql_query($link,$test) or die(mysql_error());
+			$usertester = mysql_query($link,$test);
 			echo $test;
 			echo $usertester;
 			 if(mysql_num_rows($usertester) > 0) {
