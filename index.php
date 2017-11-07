838100de655cccd19e2326c5b72721d5ed6c339b
NWretborn <niclas.vretborn@gmail.com>
2017-11-07 20:13:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -72,12 +72,7 @@
 		//window.location.replace("map.php");
 	}
 	
-	function signOut() {
-    		var auth2 = gapi.auth2.getAuthInstance();
-    		auth2.signOut().then(function () {
-      		console.log('User signed out.');
-    		});
-  	}
+	
 
 </script>
 	
@@ -86,7 +81,6 @@
 	
 
 
-<a href="index.php" onclick="signOut();">Sign out</a>
 	
 
 	<?php
