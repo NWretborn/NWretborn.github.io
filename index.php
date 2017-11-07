675abdb8d7cfe8a122743689da2dbdda9999deff
NWretborn <niclas.vretborn@gmail.com>
2017-11-07 23:54:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,6 @@
 	
 	function logout(){
 		
-		var profile = googleUser.getBasicProfile();
 		var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
