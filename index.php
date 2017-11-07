1ced3b343ae730c0cef5e650f64d3ddb50402c02
NWretborn <niclas.vretborn@gmail.com>
2017-11-07 19:38:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -63,34 +63,30 @@
 		);
 	}
 	
-
-</script>
-	
-	
-<body>
-	<div class="g-signin2" data-onsuccess="onSignIn"></div>
-
-<a href="index.php#" onclick="signOut();">Sign out</a>
-<script>
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
-</script>
-
-	
-<script>
 	function onSignIn(googleUser) {
   		var profile = googleUser.getBasicProfile();
   		console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   		console.log('Name: ' + profile.getName());
   		console.log('Image URL: ' + profile.getImageUrl());
   		console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-		window.location.replace("map.php");
+		//window.location.replace("map.php");
 	}
-	</script>
+	
+	function signOut() {
+    		var auth2 = gapi.auth2.getAuthInstance();
+    		auth2.signOut().then(function () {
+      		console.log('User signed out.');
+    		});
+  	}
+
+</script>
+	
+	
+<body>
+	
+<div class="g-signin2" data-onsuccess="onSignIn"></div>
+
+<a href="index.php" onclick="signOut();">Sign out</a>
 	
 
 	<?php
