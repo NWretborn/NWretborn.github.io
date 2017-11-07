3142e40162df14678c2f90b6aad33c3e1165e0b6
NWretborn <niclas.vretborn@gmail.com>
2017-11-07 23:14:50 +0100

    Update map.php

--- map.php
+++ map.php
@@ -121,6 +121,14 @@
 		}
  		
  	}
+		
+	function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
  	
 </script>
 <body onload="load(); openTab(event, 'viewNetwork');">
