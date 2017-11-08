d2976c9aa274e6a33f2ac09345bd9f65af50468f
NWretborn <niclas.vretborn@gmail.com>
2017-11-08 15:21:19 +0100

    Update map.php

--- map.php
+++ map.php
@@ -266,20 +266,23 @@
 
 	<script>
 function openTab(evt, tabName) {
- 
-  var holder = String(tabName);
-  var i, x, tablinks;
-  x = document.getElementsByClassName("tab");
-  for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" w3-cyan", "");
-  }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " w3-cyan";
+ 	var holder = String(tabName);
+  	var i, x, tablinks;
+  	x = document.getElementsByClassName("tab");
+  	for (i = 0; i < x.length; i++) {
+      		x[i].style.display = "none";
+  	}
+	
+  	tablinks = document.getElementsByClassName("tablink");
+	
+  	for (i = 0; i < x.length; i++) {
+      		tablinks[i].className = tablinks[i].className.replace(" w3-cyan", "");
+  	}
+	
+  	document.getElementById(tabName).style.display = "block";
+  	evt.currentTarget.className += " w3-cyan";
 }
+		
 </script>
 	
 	
