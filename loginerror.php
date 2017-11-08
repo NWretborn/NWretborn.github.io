8d3e8c747dc159cd8fd4a4fa51609217b1036466
NWretborn <niclas.vretborn@gmail.com>
2017-11-08 01:14:26 +0100

    Update loginerror.php

--- loginerror.php
+++ loginerror.php
@@ -17,10 +17,15 @@
 	<script type="text/javascript" src="jquery-3.1.1.min.js"></script>
 	<script src="https://apis.google.com/js/platform.js" async defer></script>
 
-	
 </head>
 
 <script>
+
+	function onLoad() {
+      gapi.load('auth2', function() {
+        gapi.auth2.init();
+      });
+    }
 	
 	// post the submission form to API via ajax in json format
 	function loginForm(){
@@ -42,7 +47,7 @@
         					window.location.href="loginerror.php";
     					}
 					else{
-						window.location.href="index.php";
+						location.reload();
 					}
 				},
 				dataType: "json",
@@ -57,23 +62,60 @@
 	}
 	
 	function logout(){
+		
+		var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+		
+		
 		$.post( "api.php/logout", { logout: "logoutplease"} ).done( function(){
 			location.reload();}	
 		);
 	}
+	function onSignIn(googleUser) {
+    		var profile = googleUser.getBasicProfile();
+		var user = profile.getName();
+     if(profile.getEmail()!="") {
+      		var myKeyVals = { token : googleUser.getAuthResponse().id_token }
+
+  		window.location.href = "index.php?name=" + user; 
+	
+			
+		}
+		
+    
+  }
+	function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+	
+
 </script>
 	
 	
 <body>
+	
+
+
+	
 
 	<?php
+		$GUser = $_GET['name'];
+		if($GUser != ""){
+			$_SESSION['username'] = $GUser;
+			header("Location: /index.php");
+		}
 		if(isset( $_SESSION['username'] ) ){
 			include("map.php");
 		}
 		else{
-			include("login_box_error.php");
+			include("login_box.php");
 		}
-		echo "hello php-world";
+		
 	
 	?>
 	
@@ -87,6 +129,7 @@
 </html>
 
 <!--
+
 	style="top: 12%; left: 8%"			style="top: 12%"
 	style="top: 32%; left: 8%;"			style="top: 32%"
 	style="top:65%; left: 89%;"			style="top:65%; left: 58%;"
