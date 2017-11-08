5b7f718d93dfa23c48e40ac8a05bb18c42666fcd
NWretborn <niclas.vretborn@gmail.com>
2017-11-08 14:48:45 +0100

    Update api_documentation.php

--- api_documentation.php
+++ api_documentation.php
@@ -11,36 +11,57 @@
 	<h1>API Documentation</h1>
 
 	<section id="adduser">
-		<h3>adduser</h3>
+		<h2>adduser</h2>
 		<p>Adds a user via a json object</p>
 		<h5>Method</h5>
+		<p>GET</p>
+		<h5>Sample Call</h5>
 		<p>GET</p>
 	</section>
 
 	<section id="addwifi">
-		<h3>addwifi</h3>
-		<p>The information you upload to our website will be used for the purpose of the application and other users of the application. We may use all data uploaded to our website for testing purposes.</p>
+		<h2>addwifi</h2>
+		<p>Adds a user via a json object</p>
+		<h5>Method</h5>
+		<p>GET</p>
+		<h5>Sample Call</h5>
+		<p>GET</p>
 		</section>
 
 	<section id="deletewifi">
-		<h3>deletewifi</h3>
-		<p>We do our best at protecting your confidential information, but we do not guarantee that any information you upload to our website will be secured in any way.</p>
+		<h2>deletewifi</h2>
+		<p>Adds a user via a json object</p>
+		<h5>Method</h5>
+		<p>GET</p>
+		<h5>Sample Call</h5>
+		<p>GET</p>
 	</section>
 
 	<section id="wifipic">
-		<h3>wifipic</h3>
-		<p>Yes. Cookies are small files that a site or its service provider transfers to your computers hard drive through your Web browser (if you allow them) that enable the sites or service providers systems to recognize your browser and capture and remember certain information</p>
-		<p>If you prefer, you can choose to have your computer warn you each time a cookie is being sent, or you can choose to turn off all cookies via your browser settings. Like most websites, if you turn your cookies off, some of our services may not function properly.</p>
+		<h2>wifipic</h2>
+		<p>Adds a user via a json object</p>
+		<h5>Method</h5>
+		<p>GET</p>
+		<h5>Sample Call</h5>
+		<p>GET</p>
 	</section>
 
 	<section id="login">
-		<h3>login</h3>
-		<p>We do not sell or trade any of the information uploaded to our site. We may however use the information uploaded to our website in third-party applications.</p>
+		<h2>login</h2>
+		<p>Adds a user via a json object</p>
+		<h5>Method</h5>
+		<p>GET</p>
+		<h5>Sample Call</h5>
+		<p>GET</p>
 	</section>
 
 	<section id="logout">
-		<h3>logout</h3>
-		<p>Occasionally, at our discretion, we may include or offer third party products or services on our website. These third party sites have separate and independent privacy policies. We therefore have no responsibility or liability for the content and activities of these linked sites.</p>
+		<h2>logout</h2>
+		<p>Adds a user via a json object</p>
+		<h5>Method</h5>
+		<p>GET</p>
+		<h5>Sample Call</h5>
+		<p>GET</p>
 	</section>
 	</span>
 	</div>
