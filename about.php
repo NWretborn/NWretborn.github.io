70dd3a805d9fbd830e00399753092215a77e1015
NWretborn <niclas.vretborn@gmail.com>
2017-11-08 15:11:11 +0100

    Update about.php

--- about.php
+++ about.php
@@ -23,7 +23,7 @@
 			<p class="subtext">
 			Niclas Vretborn<br/>Luleå Tekniska Universitet
 			</p>
-			</br>
+			</br></br>
 			<span id="logo" class="git" onclick="location.href='https://github.com/NWretborn/WiFinder'"> </span>
 			<br/><br/>
 		</span>
