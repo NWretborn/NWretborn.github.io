6531cf6e98f210f64209cc6d0dca86a171bed4ac
NWretborn <niclas.vretborn@gmail.com>
2017-11-08 14:39:18 +0100

    Update api_documentation.php

--- api_documentation.php
+++ api_documentation.php
@@ -1,46 +1,50 @@
-case 'adduser':
-			$table='user';
-			$curuser = $input['name'];
-			if($input['password']){
-				$input['password'] = hashpass($input['password']);
-			}
-			
-			badcall($method!='POST', "use POST for adduser"); //NOT SURE IF BADCALL IS GOOD METHOD
-			break;
-		case 'addwifi':
-			$table='markers';
-			badcall($method!='POST', "use POST for addwifi");
-			break;
-		case 'deletewifi':
-			badcall($method!='POST', "use POST for deletewifi");
-			if(($_SESSION['username'] == $input['user']) or ($_SESSION['username'] == 'admin')){
-				$table='markers';
-				$id='picurl';
-				$key= $input['picurl'];
-				$method = 'DELETE';
-				break;
-			}
-			else{
-				badcall(True, "Only your own networks can be removed");
-				break;
-			}
-		case 'wifipic':
-			errlog("POST picname: ".$_POST['picname']);
-			if(isset($_FILE['file']))
-				errlog("FILE is set!");
-			include('upload.processor.php');
-			exit();
-			break;
-		case 'login':
-			badcall($method!='GET', "use GET for login");
-			$table='user';
-			$id='name';
-			break;
-		case 'logout':
-			errlog("logging out user: ".$_SESSION['username']);
-			unset($_SESSION['username']);
-			exit();
-			break;
-		default:
-			badcall(True, "not an api-function");
-			break;
+<html>
+
+<head>
+	<title>WiFinder</title>
+	<link rel="stylesheet" type="text/css" href="libs/wifinder.css">
+</head>
+
+<body>
+	<div class="about_box center" style="height:70vh;">
+	<span class="text_box center">
+	<h1>API Documentation</h1>
+
+	<section id="adduser">
+		<h3>What information do we collect?</h3>
+		<p>We collect information from you when you register on our site, fill out a form or when you explicitly upload data to our website. We might collect and store data without you knowing about it.</p>
+	</section>
+
+	<section id="addwifi">
+		<h3>What do we use your information for?</h3>
+		<p>The information you upload to our website will be used for the purpose of the application and other users of the application. We may use all data uploaded to our website for testing purposes.</p>
+		</section>
+
+	<section id="deletewifi">
+		<h3>How do we protect your information?</h3>
+		<p>We do our best at protecting your confidential information, but we do not guarantee that any information you upload to our website will be secured in any way.</p>
+	</section>
+
+	<section id="wifipic">
+		<h3>Do we use cookies?</h3>
+		<p>Yes. Cookies are small files that a site or its service provider transfers to your computers hard drive through your Web browser (if you allow them) that enable the sites or service providers systems to recognize your browser and capture and remember certain information</p>
+		<p>If you prefer, you can choose to have your computer warn you each time a cookie is being sent, or you can choose to turn off all cookies via your browser settings. Like most websites, if you turn your cookies off, some of our services may not function properly.</p>
+	</section>
+
+	<section id="login">
+		<h3>Do we disclose any information to outside parties?</h3>
+		<p>We do not sell or trade any of the information uploaded to our site. We may however use the information uploaded to our website in third-party applications.</p>
+	</section>
+
+	<section id="logout">
+		<h3>Third party links</h3>
+		<p>Occasionally, at our discretion, we may include or offer third party products or services on our website. These third party sites have separate and independent privacy policies. We therefore have no responsibility or liability for the content and activities of these linked sites.</p>
+	</section>
+	</span>
+	</div>
+	
+	<span id="logo" class="about" onclick="location.href='index.php'"> </span>
+	<span class="cache" style="background: url(img/logo_blue.png)"></span>
+</body>
+
+</html>
