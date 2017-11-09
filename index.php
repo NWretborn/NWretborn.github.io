f1a51220fa7aee50e34d5b69ffc03b4903edebf7
NWretborn <niclas.vretborn@gmail.com>
2017-11-09 18:44:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -99,7 +99,7 @@
 	
 <body>
 	
-	<p style="font-size: var(--FONT_HEIGHT); font-family: monospace;">WiFinder</p>
+	<p style="font-size: 40p; font-family: monospace; color: #10454e;">WiFinder</p>
  
 	
 
