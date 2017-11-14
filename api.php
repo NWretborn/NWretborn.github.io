723be7f8ee36f7cbf06c19edeb9cacc33cc71310
NWretborn <niclas.vretborn@gmail.com>
2017-11-14 15:46:36 +0100

    Update api.php

--- api.php
+++ api.php
@@ -1,7 +1,5 @@
 <?php
 	session_start();
-	//session_start() creates a session or resumes the current one based on a session identifier 
-	//passed via a GET or POST request,or passed via a cookie.
 ?>
 
 <?php
