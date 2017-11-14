95f6e799638e6928ba7e9fb738344d7debd76c93
NWretborn <niclas.vretborn@gmail.com>
2017-11-14 15:52:50 +0100

    Update api.php

--- api.php
+++ api.php
@@ -1,21 +1,23 @@
 <?php
+	//session_start() creates a session or resumes the current one based on a session identifier 
+	//passed via a GET or POST request,or passed via a cookie. 
 	session_start();
 ?>
 
 <?php
-	#Sets the charset to UTF8, this speciefies how characters are to be used within the API.
-	#It also starts an error log that saves any errors recorded in a .txt file on the server.
+	//Sets the charset to UTF8, this speciefies how characters are to be used within the API.
+	//It also starts an error log that saves any errors recorded in a .txt file on the server.
 	mysql_set_charset("UTF8");
 	ini_set("allow_url_fopen", true);
 	ini_set("log_errors", 1);
 	ini_set("error_log", "./error.log");
-	#Default id to go through database
+	//Default id to go through database
 	$id='id';
-	#Function for writing errors to error log
+	//Function for writing errors to error log
 	function errlog($message){
 		error_log($message."\n", 3, "./scrap2.log");
 	}
-	#Badcall function, returns a 404 http response code(not found), if wrong method is used.
+	//Badcall function, returns a 404 http response code(not found), if wrong method is used.
 	function badcall($isbad, $message){
 		if($isbad){
 			errlog($message);
@@ -24,42 +26,42 @@
 		}
 	}
 	
-	#Function for hashing(encrypting) the users password
+	//Function for hashing(encrypting) the users password
 	function hashpass($password){
 		return password_hash($password, PASSWORD_DEFAULT);
 	}
-	#Function for verifying password when signing in	
+	//Function for verifying password when signing in	
 	function verifypass($password, $hashed_password){
 		return password_verify($password, $hashed_password);
 	}
 ?>
 
 <?php
-	#Writes to the errorlog and includes the database information file
+	//Writes to the errorlog and includes the database information file
 	errlog("STARTLOG");
 	errlog("--------------------------------------------------");
 	require("phpsqlajax_dbinfo.php");
-	#Get the HTTP method(POST, GET, PUT or DELETE)
+	//Get the HTTP method(POST, GET, PUT or DELETE)
 	$method = $_SERVER['REQUEST_METHOD'];
-	#Post information to errorlog
+	//Post information to errorlog
 	errlog($_SERVER['PHP_SELF']." received ".$method." request");
-	#Sets the path to a variable, e.g. adduser, addwifi etc
+	//Sets the path to a variable, e.g. adduser, addwifi etc
 	$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 	
-	#Assigns the JSON object to a php-variable and decodes it into the right format.
+	//Assigns the JSON object to a php-variable and decodes it into the right format.
 	$input = file_get_contents('php://input');
 	$input = json_decode($input,true);
-	#Creates a conenction to the database using variables found in the phpsqlajax_dbinfo.php file
+	//Creates a conenction to the database using variables found in the phpsqlajax_dbinfo.php file
 	$link = mysqli_connect('localhost', $username, $password, $database);
 	mysqli_set_charset($link,'utf8');
-	#Retrieve table and key from the path remove from variable $request
+	//Retrieve table and key from the path remove from variable $request
 	$apicall = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
 	$key = array_shift($request);
-	#Uses apicall to determine witch function was called.
+	//Uses apicall to determine witch function was called.
 	switch($apicall){
 		
-		#This function is for adding a user. It checks for the correct method, sets the table
-		#to user and hashes the password.
+		//This function is for adding a user. It checks for the correct method, sets the table
+		//to user and hashes the password.
 		case 'adduser':
 			
 			badcall($method!='POST', "use POST for adduser");
@@ -71,17 +73,17 @@
 			}
 			break;
 		
-		#This function is for adding wifi networks. It checks for the correct method and 
-		#sets the table to markers
+		//This function is for adding wifi networks. It checks for the correct method and 
+		//sets the table to markers
 		case 'addwifi':
 			
 			badcall($method!='POST', "use POST for addwifi");
 			$table='markers';
 			break;
 			
-		#This function is for deleting a wifi network. It sets the table to markers and the id to the picurl.
-		#Wifi networks are distinguished by their key, which is a combination of the latitude and longitude coordinates
-		#saved in a string. It then sets the method to delete.
+		//This function is for deleting a wifi network. It sets the table to markers and the id to the picurl.
+		//Wifi networks are distinguished by their key, which is a combination of the latitude and longitude coordinates
+		//saved in a string. It then sets the method to delete.
 		case 'deletewifi':
 			
 			badcall($method!='POST', "use POST for deletewifi");
@@ -99,8 +101,8 @@
 				break;
 			}
 			
-		#This functions adds a picture to a wifi network. It includes the upload.processor.php
-		#used to upload files to the server.
+		//This functions adds a picture to a wifi network. It includes the upload.processor.php
+		//used to upload files to the server.
 		case 'wifipic':
 			
 			errlog("POST picname: ".$_POST['picname']);
@@ -112,8 +114,8 @@
 			exit();
 			break;
 			
-		#This function is for logging in, it checks for the correct method and
-		#sets the table to the user table and the ID for the user name
+		//This function is for logging in, it checks for the correct method and
+		//sets the table to the user table and the ID for the user name
 		case 'login':
 			
 			badcall($method!='GET', "use GET for login");
@@ -121,7 +123,7 @@
 			$id='name';
 			break;
 		
-		#This function is for logging out, it finalizes here and destroys the client session.
+		//This function is for logging out, it finalizes here and destroys the client session.
 		case 'logout':
 			
 			errlog("logging out user: ".$_SESSION['username']);
@@ -129,30 +131,30 @@
 			exit();
 			break;
 		
-		#If the call is not one of the switch cases, the api returns a bad call.
+		//If the call is not one of the switch cases, the api returns a bad call.
 		default:
 			badcall(True, "not an api-function");
 			break;
 			
 	}
-	#This function is used to create a legal SQL string. The given string is encoded to 
-	#an escaped SQL string, taking into account the current character set of the connection.
-	#This prevents SQL-injection attacks from potential hackers.
+	//This function is used to create a legal SQL string. The given string is encoded to 
+	//an escaped SQL string, taking into account the current character set of the connection.
+	//This prevents SQL-injection attacks from potential hackers.
 	$columns = preg_replace('/[^a-z0-9_]+/i','',array_keys($input));
 	$values = array_map(function ($value) use ($link) {
 		if ($value===null) return null;
 		return mysqli_real_escape_string($link,(string)$value);
 	},array_values($input));
 	
-	#The 'set' variable is used when updating and inserting into the database. It contains
-	#all the information from the JSON object, e.g. username, password, e-mail etc.
+	//The 'set' variable is used when updating and inserting into the database. It contains
+	//all the information from the JSON object, e.g. username, password, e-mail etc.
 	$set = '';
 	for ($i=0;$i<count($columns);$i++) {
 		$set.=($i>0?',':'').'`'.$columns[$i].'`=';
 		$set.=($values[$i]===null?'NULL':'"'.$values[$i].'"');
 	}
 	
-	#Depending on the http method used, this switch forms the apropriate SQL statement
+	//Depending on the http method used, this switch forms the apropriate SQL statement
 	switch ($method) {
 		case 'GET':
 			$sql = "select * from `$table`".($key?" WHERE $id='$key'":'');
@@ -167,11 +169,11 @@
 			$sql = "delete from `$table` where $id='$key'";
 			break;
 	}
-	#Logs the method used
+	//Logs the method used
 	errlog("sql {".$sql."} sent");
 	
-	#Here, the API finalizes the call and sends the request to the database, it also
-	#writes errors to the errorlog and 
+	//Here, the API finalizes the call and sends the request to the database, it also
+	//writes errors to the errorlog and 
 	$result = mysqli_query($link,$sql);
 	$errors = mysqli_error($link);
 	if($errors){
@@ -182,14 +184,14 @@
 	}
 	}
 	
-	#If the SQL statement fails, kills the connection
+	//If the SQL statement fails, kills the connection
 	if (!$result) {
 		errlog("sql no result");
 		http_response_code(404);
 		die(mysqli_error());
 	}
-	#This code is for documentation only. If SQL statement is succesful, returns a text with
-	#succesful data input. If not, writes it to error log.
+	//This code is for documentation only. If SQL statement is succesful, returns a text with
+	//succesful data input. If not, writes it to error log.
 	$resultarray = array();
 	if ($method == 'GET') {
 		if (!$key) echo '[';
@@ -208,10 +210,10 @@
 		errlog("affected rows: ".mysqli_affected_rows($link) );
 	}
 	
-	#The API is done and closes the link
+	//The API is done and closes the link
 	mysqli_close($link);
-	#This switch case is for changes to backend without the need for a SQL statement.
-	#An example of this is the login function, it just sets the session, no need for SQL!
+	//This switch case is for changes to backend without the need for a SQL statement.
+	//An example of this is the login function, it just sets the session, no need for SQL!
 	switch($apicall){
 			
 		case 'adduser':
@@ -246,7 +248,7 @@
 			break;
 			
 	}
-	#Ends error log segment
+	//Ends error log segment
 	errlog("--------------------------------------------------");
 	errlog("ENDLOG");
 ?>
