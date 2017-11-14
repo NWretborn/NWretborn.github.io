f7eee1b0ca05dc5c5cbfe83b9ccc4125c14f4bff
NWretborn <niclas.vretborn@gmail.com>
2017-11-14 15:44:48 +0100

    Update api.php

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php
-	#session_start() creates a session or resumes the current one based on a session identifier 
-	#passed via a GET or POST request,or passed via a cookie. 
+	//session_start() creates a session or resumes the current one based on a session identifier 
+	//passed via a GET or POST request,or passed via a cookie.
 	session_start();
 ?>
 
