feb3d52d2c3026a1be8d3d19f55e8b503778980a
NWretborn <niclas.vretborn@gmail.com>
2017-11-14 15:31:46 +0100

    Update api.php

--- api.php
+++ api.php
@@ -1,41 +1,23 @@
 <?php
+	#session_start() creates a session or resumes the current one based on a session identifier 
+	#passed via a GET or POST request,or passed via a cookie. 
 	session_start();
 ?>
 
-<!--  
-	login:
-		url:	api.php/login/name 	//where name is username, it will be added with quotation marks (works with ID)
-		data: 	password sent to $_GET['password']. Sending a json object with {password: "yourPassword"} works
-	logout:
-		url: 	api.php/logout
-	adduser:
-		url: 	api.php/adduser
-		data:	json string with columns 'name', 'email' and 
-			'password' with corresponding values.
-	addwifi:
-		url: 	api.php/addwifi
-		data: 	...add more info here...
-	wifipic:
-		method:	POST
-		url: 	api.php/wifipic
-		data:	send picname with $_POST['picname'] and picture with $_FILE['file']
--->
-
 <?php
+	#Sets the charset to UTF8, this speciefies how characters are to be used within the API.
+	#It also starts an error log that saves any errors recorded in a .txt file on the server.
 	mysql_set_charset("UTF8");
 	ini_set("allow_url_fopen", true);
 	ini_set("log_errors", 1);
 	ini_set("error_log", "./error.log");
-	#ini_set('display_errors', 1);
-	#ini_set('display_startup_errors', 1);
-	
-	// default id to go through database
+	#Default id to go through database
 	$id='id';
-	// error-message written to a scrap log in same folder
+	#Function for writing errors to error log
 	function errlog($message){
 		error_log($message."\n", 3, "./scrap2.log");
 	}
-	// VERY brutal end-script function
+	#Badcall function, returns a 404 http response code(not found), if wrong method is used.
 	function badcall($isbad, $message){
 		if($isbad){
 			errlog($message);
@@ -43,35 +25,39 @@
 			die($message);
 		}
 	}
+	
+	#Function for hashing(encrypting) the users password
 	function hashpass($password){
 		return password_hash($password, PASSWORD_DEFAULT);
 	}
-	
+	#Function for verifying password when signing in	
 	function verifypass($password, $hashed_password){
 		return password_verify($password, $hashed_password);
 	}
 ?>
 
 <?php
+	#Writes to the errorlog and includes the database information file
 	errlog("STARTLOG");
 	errlog("--------------------------------------------------");
 	require("phpsqlajax_dbinfo.php");
-	// get the HTTP method, path and body of the request
+	#Get the HTTP method(POST, GET, PUT or DELETE)
 	$method = $_SERVER['REQUEST_METHOD'];
+	#Post information to errorlog
 	errlog($_SERVER['PHP_SELF']." received ".$method." request");
+	#Sets the path to a variable, e.g. adduser, addwifi etc
 	$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 	
+	#Assigns the JSON object to a php-variable and decodes it into the right format.
 	$input = file_get_contents('php://input');
-	//errlog("input: $input");
 	$input = json_decode($input,true);
-	//errlog("get: ".$_GET['password']);
-	// connect to the mysql database
+	#Creates a conenction to the database using variables found in the phpsqlajax_dbinfo.php file
 	$link = mysqli_connect('localhost', $username, $password, $database);
 	mysqli_set_charset($link,'utf8');
-	// retrieve table and key from the path remove from variable $request
+	#Retrieve table and key from the path remove from variable $request
 	$apicall = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
-	$key = array_shift($request);	// "array_shift($request)+0" before
-	// initial modifications based on what call is made
+	$key = array_shift($request);
+	#Uses apicall to determine witch function was called.
 	switch($apicall){
 		case 'adduser':
 			$table='user';
